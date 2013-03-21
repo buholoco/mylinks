d82bd071944035898db0686d86e51b4cac7ce505
buholoco <buholoco@gmail.com>
2013-03-21 16:04:47 -0300

    Cleaning up Acme bundle and added my own

---
+++ src/Buho/MylinksBundle/BuhoMylinksBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Buho\MylinksBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BuhoMylinksBundle extends Bundle
+{
+}
---
+++ src/Buho/MylinksBundle/Controller/HomeController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Buho\MylinksBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class HomeController extends Controller
+{
+    
+    public function indexAction()
+    {
+        return $this->render('BuhoMylinksBundle:Home:index.html.twig');
+    }
+}
